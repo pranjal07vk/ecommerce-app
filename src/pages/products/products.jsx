115cd9de03500df4656354f6416844c76d691164
pranjal07vk <pranjal07vk@gmail.com>
2026-03-27 02:27:19 +0530

    connected products page to api

--- src/pages/products/products.jsx
+++ src/pages/products/products.jsx
@@ -1,5 +1,22 @@
+import useProducts from "../../hooks/useProducts";
+
 function Products() {
-  return <h2>Products Page</h2>;
+  const { products, loading } = useProducts();
+
+  if (loading) return <h2>Loading...</h2>;
+
+  return (
+    <div>
+      <h2>All Products</h2>
+
+      {products.map((product) => (
+        <div key={product.id}>
+          <h4>{product.title}</h4>
+          <p>₹{product.price}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default Products;
