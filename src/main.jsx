f96238b38d842d4303d9de8b80bee35aaf68231d
pranjal07vk <pranjal07vk@gmail.com>
2026-03-27 02:54:37 +0530

    updated main.jsx accordingly

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import CartProvider from "./context/cartContext";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <CartProvider>
+      <App />
+    </CartProvider>
+  </React.StrictMode>
+);
