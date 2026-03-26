c26d06c5fb1edc5a992ce767c6786fed10f536fe
pranjal07vk <pranjal07vk@gmail.com>
2026-03-27 02:44:29 +0530

    added a slight delay in search results

--- src/pages/products/products.jsx
+++ src/pages/products/products.jsx
@@ -1,18 +1,20 @@
 import useProducts from "../../hooks/useProducts";
 import ProductCard from "../../components/ProductCard/ProductCard";
 import { useState } from "react";
+import useDebounce from "../../hooks/useDebounce";
 
 function Products() {
   const { products, loading } = useProducts();
   const [search, setSearch] = useState("");
+  const debouncedSearch = useDebounce(search, 500);
   const filteredProducts = products.filter((product) =>
-  product.title.toLowerCase().includes(search.toLowerCase())
-);
+    product.title.toLowerCase().includes(debouncedSearch.toLowerCase())
+  );
 
   if (loading) return <h2>Loading...</h2>;
 
   return (
-    <div style={{ display: "grid", gridTemplateColumns: "repeat(3, 1fr)", gap: "20px" }}>
+    <div>
       <input
         type="text"
         placeholder="Search products..."
@@ -22,9 +24,13 @@ function Products() {
 
       <h2>All Products</h2>
 
-      {filteredProducts.map((product) => (
-        <ProductCard key={product.id} product={product} />
-      ))}
+      <div style={{ display: "grid", gridTemplateColumns: "repeat(3, 1fr)", gap: "20px" }}>
+        {filteredProducts.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
+
+      
     </div>
   );
 }
